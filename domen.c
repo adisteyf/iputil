b62bd96923a449c4cbeff7b5d5784aa623f42987
adisteyf <adisteyf@gmail.com>
2025-05-16 12:25:18 +0300

    added more options

--- domen.c
+++ domen.c
@@ -6,6 +6,36 @@
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <strings.h>
+
+
+static char *
+get_protocol (int num)
+{
+	switch (num) {
+		case IPPROTO_UDP:
+			return "udp";
+		case IPPROTO_TCP:
+			return "tcp";
+
+		default:
+			return "???";
+	}
+}
+
+static char *
+get_socktype (int num)
+{
+	switch (num) {
+		case SOCK_STREAM:
+			return "SOCK_STREAM";
+		case IPPROTO_TCP:
+			return "IPPROTO_TCP";
+
+		default:
+			return "???";
+	}
+}
 
 void
 get_domen (const char * domen)
@@ -14,7 +44,7 @@ get_domen (const char * domen)
 	struct addrinfo hints, *p;
 	char            ipstr[INET6_ADDRSTRLEN];
 
-	memset(&hints,0,sizeof(hints));
+	bzero(&hints,sizeof(hints));
 	hints.ai_family   = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 
@@ -31,19 +61,19 @@ get_domen (const char * domen)
 
 		if (p->ai_family == AF_INET) {
 			struct sockaddr_in * ipv4 = (struct sockaddr_in *)p->ai_addr;
-			addr = &(ipv4->sin_addr);
-			ipver = "IPv4";
+			addr       = &(ipv4->sin_addr);
+			ipver      = "IPv4";
 		} else if (p->ai_family == AF_INET6) {
 			struct sockaddr_in6 * ipv6 = (struct sockaddr_in6 *)p->ai_addr;
-			addr = &(ipv6->sin6_addr);
-			ipver = "IPv6";
+			addr       = &(ipv6->sin6_addr);
+			ipver      = "IPv6";
 		} else {
 			puts("  ERR: unsupported ai_family");
 			continue;
 		}
 
 		inet_ntop(p->ai_family,addr,ipstr,sizeof(ipstr));
-		printf("  %s: %s\n",ipver,ipstr);
+		printf("  %s: %s (%s) | sock: %s, flags: %d\n",ipver,ipstr,get_protocol(p->ai_protocol),get_socktype(p->ai_socktype),p->ai_flags);
 		p=p->ai_next;
 	}
 
